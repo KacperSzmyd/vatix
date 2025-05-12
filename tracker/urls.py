c5a738bc0c9f20ecdd05011a317fbbcbb380090e
Kacper <k.g.szmyd@gmail.com>
2025-05-12 17:55:06 +0200

    Add endpoint for saving localiation

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import AssigDeviceView
+from .views import AssigDeviceView, DeviceLocalizationView
 
 urlpatterns = [
-    path("devices/<str:id>/assign", AssigDeviceView.as_view(), name="assign-device")
+    path("devices/<str:id>/assign", AssigDeviceView.as_view(), name="assign-device"),
+    path(
+        "devices/<str:id>/localization",
+        DeviceLocalizationView.as_view(),
+        name="device-localization",
+    ),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from .serializers import DeviceAssignSerializer
+from .serializers import DeviceAssignSerializer, LocalizationSerializer
 from .models import Device
 
 
@@ -16,4 +16,23 @@ class AssigDeviceView(APIView):
         if serializer.is_valid():
             serializer.save(device=device)
             return Response({"status": "Device assigned"}, status=200)
+        return Response(serializer.errors, status=400)
+
+
+class DeviceLocalizationView(APIView):
+    def post(self, request, id):
+        try:
+            device = Device.objects.get(device_id=id)
+        except Device.DoesNotExist:
+            return Response({"error": "Device not found."}, status=404)
+
+        if device.user is None:
+            return Response({"error": "Device is not assigned to ny user."}, status=400)
+
+        serializer = LocalizationSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save(device=device)
+            return Response({"status": "Localization recorded"}, status=201)
+
         return Response(serializer.errors, status=400)
