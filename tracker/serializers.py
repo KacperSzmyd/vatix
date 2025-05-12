501e436db65e4b6bca4cbbfc7ccd7cd2d1ebd61a
Kacper <k.g.szmyd@gmail.com>
2025-05-12 17:03:26 +0200

    Add DeviceAssign serializer

---
+++ tracker/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import User, Device
+
+class DeviceAssignSerializer(serializers.Serializer):
+    user_id = serializers.IntegerField()
+    
+    def validate_user_id(self, value):
+        try:
+            user = User.objects.get(id=value)
+        except User.DoesNotExist:
+            raise serializers.ValidationError("User not Found.")
+        return value
+        
+    def save(self, device):
+        user_id = self.validated_data['user_id']
+        new_user = User.objects.get(id=user_id)
+        
+        if hasattr(new_user, "device"):
+            old_device = new_user.device
+            old_device.user = None
+            old_device.save()
+            
+        device.user = new_user
+        device.save()
+        return device
