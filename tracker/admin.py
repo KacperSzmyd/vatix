474e3f7c5e5af9e2149959637fa6854ee9897799
Kacper <k.g.szmyd@gmail.com>
2025-05-12 16:40:09 +0200

    Add User, Device and Localization models

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, Device, Localization
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Device)
+admin.site.register(Localization)
--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=50, blank=False, null=False)
+    last_name = models.CharField(max_length=50, blank=False, null=False)
+
+    def __str__(self):
+        return f"{self.first_name.title()} {self.last_name.title()}"
+
+
+class Device(models.Model):
+    device_id = models.CharField(max_length=50, unique=True)
+    user = models.OneToOneField(
+        User, null=True, blank=True, on_delete=models.SET_NULL, related_name="device"
+    )
+
+    def __str__(self):
+        return str(self.device_id)
+
+
+class Localization(models.Model):
+    # Optional on_delete=models.CASCADE in case when historical location data is irrelevant
+    device = models.ForeignKey(
+        Device,
+        on_delete=models.SET_NULL,
+        related_name="locations",
+        blank=True,
+        null=True,
+    )
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    ping_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.device.device_id} @ {self.ping_time}"
